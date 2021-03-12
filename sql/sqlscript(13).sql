4b25006c4407de60831ffffc053e2695bb0af8ab
Uday Verma <76985181+uverma94@users.noreply.github.com>
2021-03-12 19:21:38 +0530

    Activity 6

---
+++ sql/sqlscript(13).sql
@@ -0,0 +1,16 @@
+REM   Script: Session 6
+REM   Activity 6
+
+desc salesman 
+
+
+create table salesman(  
+    salesman_id int not null,  
+    salesman_name varchar2(20),  
+    salesman_city varchar2(20),  
+    commission int  
+);
+
+desc salesman 
+
+
